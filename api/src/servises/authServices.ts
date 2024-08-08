82d212c1b78a004c3e3c9792a320b3cf1f9fa618
j-kimura-dcworks <j.kimura@dcworks.jp>
2024-08-08 20:38:31 +0900

    authServices.ts作成

---
+++ api/src/servises/authServices.ts
@@ -0,0 +1,45 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { prisma } from '../app';
+import { config } from '../config/environment';
+
+// ユーザー登録サービス
+export const registerUser = async (
+  username: string,
+  email: string,
+  password: string
+) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+    select: { id: true, username: true, email: true },
+  });
+
+  return user;
+};
+
+// ユーザーログインサービス
+export const loginUser = async (email: string, password: string) => {
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    throw new Error('Invalid credentials');
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw new Error('Invalid credentials');
+  }
+
+  const token = jwt.sign({ userId: user.id }, config.SECRET_KEY!, {
+    expiresIn: '1d',
+  });
+
+  return {
+    token,
+    user: { id: user.id, username: user.username, email: user.email },
+  };
+};
